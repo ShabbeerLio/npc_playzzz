79d524d5e128249397aaf84ab45d750f2c5bb582
Shabbeer <shabbeer@Shabbeers-MacBook-Air.local>
2026-01-07 21:45:32 +0530

    first commit

---
+++ components/Box.js
@@ -0,0 +1,66 @@
+"use client";
+import LaserFlow from "./LaserFlow";
+import { useRef } from "react";
+
+export default function Box() {
+  const revealImgRef = useRef(null);
+  return (
+    <>
+      <div
+      className="box"
+        onMouseMove={(e) => {
+          const rect = e.currentTarget.getBoundingClientRect();
+          const x = e.clientX - rect.left;
+          const y = e.clientY - rect.top;
+          const el = revealImgRef.current;
+          if (el) {
+            el.style.setProperty("--mx", `${x}px`);
+            el.style.setProperty("--my", `${y + rect.height * 0.5}px`);
+          }
+        }}
+        onMouseLeave={() => {
+          const el = revealImgRef.current;
+          if (el) {
+            el.style.setProperty("--mx", "-9999px");
+            el.style.setProperty("--my", "-9999px");
+          }
+        }}
+      >
+        <LaserFlow
+          horizontalBeamOffset={0.1}
+          verticalBeamOffset={0.0}
+          color="#570e71"
+        />
+
+        <div
+          className="box-content"
+        >
+          {/* Your content here */}
+        </div>
+
+        <img
+          ref={revealImgRef}
+          src="/path/to/image.jpg"
+          alt="Reveal effect"
+          style={{
+            position: "absolute",
+            width: "100%",
+            top: "-50%",
+            zIndex: 5,
+            mixBlendMode: "lighten",
+            opacity: 0.3,
+            pointerEvents: "none",
+            "--mx": "-9999px",
+            "--my": "-9999px",
+            WebkitMaskImage:
+              "radial-gradient(circle at var(--mx) var(--my), rgba(255,255,255,1) 0px, rgba(255,255,255,0.95) 60px, rgba(255,255,255,0.6) 120px, rgba(255,255,255,0.25) 180px, rgba(255,255,255,0) 240px)",
+            maskImage:
+              "radial-gradient(circle at var(--mx) var(--my), rgba(255,255,255,1) 0px, rgba(255,255,255,0.95) 60px, rgba(255,255,255,0.6) 120px, rgba(255,255,255,0.25) 180px, rgba(255,255,255,0) 240px)",
+            WebkitMaskRepeat: "no-repeat",
+            maskRepeat: "no-repeat",
+          }}
+        />
+      </div>
+    </>
+  );
+}
